c30afcbd6342b7991846b47f33760f5ceef313d1
hazeroj <153484003+hazeroj@users.noreply.github.com>
2024-04-18 21:25:04 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,29 +2,11 @@ $(function () {
     const wWith = $(window).width();
     const slide = $('#slide');
     let n, i, timer, nextSlide;
-
-
-    // 내비게이션
-    const gnb = $('.gnb>li')
+    const nav =$('nav')
+    const gnb = nav.find('.gnb>li')
     const lnb = gnb.find('.sub')
     const lnbBack = $('.back')
 
-    gnb.mouseenter(function () {
-        lnb.stop().slideDown();
-        lnbBack.stop().slideDown();
-    })
-
-    lnbBack.mouseout(function () {
-        lnb.stop().slideUp();
-        lnbBack.stop().slideUp();
-    })
-
-    lnb.mouseover(function () {
-        $(this).prev().css('font-weight', '800')
-    })
-    lnb.mouseleave(function () {
-        $(this).prev().css('font-weight', '400')
-    })
 
 
     // 슬라이드 
@@ -56,7 +38,7 @@ $(function () {
         $('.s_img').stop().animate({ 'left': -wWith * i + 'px' }, 1000)
     })
 
-    // 메뉴(커피)
+    // 메뉴
     const menu = $('#menu')
     const menuBack = menu.find('.menu_back a')
     const menuList = menu.find('.menu_list>div')
@@ -86,16 +68,81 @@ $(function () {
     // 사이드배너
     const banner = $('#banner li');
 
-    let m  = 0
+    let m = 0
     function sBanner() {
-        nextSlide = (m+1) % 3
+        nextSlide = (m + 1) % 3
 
         banner.eq(nextSlide).fadeIn(2000);
         banner.eq(m).fadeOut(2000);
-        
-        m=nextSlide;
-        
+
+        m = nextSlide;
+
     }
 
-    setInterval(sBanner,8000);
+    setInterval(sBanner, 8000);
+
+
+    // 내비게이션 효과
+    lnb.click(function () {
+        $(this).prev().css('font-weight', '800')
+    })
+    lnb.mouseout(function () {
+        $(this).prev().css('font-weight', '400')
+    })
+
+
+    // pc
+    if ($(window).width() >= 1279) {
+
+
+        // 내비게이션
+
+        gnb.mouseenter(function () {
+            lnb.stop().slideDown();
+            lnbBack.stop().slideDown();
+        })
+
+        lnbBack.mouseout(function () {
+            lnb.stop().slideUp();
+            lnbBack.stop().slideUp();
+        })
+
+
+        // 탑버튼
+
+        const topBtn = $('.top');
+
+        $(window).scroll(function () {
+            let scTop = $(this).scrollTop();
+
+            // console.log(scTop)
+            if (scTop >= 600) {
+                topBtn.fadeIn();
+            } else { topBtn.fadeOut(); }
+        })
+
+        topBtn.click(function () {
+            $(window).scrollTop(0);
+        });
+    }
+
+
+    // 태블릿
+    if($(window).width()<1279){
+
+        // 내비게이션
+        nav.find('.nav_btn ').click(function(){
+            $(this).siblings().stop().slideToggle();
+            nav.find('a:nth-child(3)').css('display','block')
+            $(this).find('span').toggleClass('c_btn');
+            lnb.slideUp();
+        })
+
+        gnb.find('a').click(function(){
+            $(this).next(lnb).slideToggle().parent().siblings().find(lnb).slideUp();
+        })
+
+
+    }
+
 })
